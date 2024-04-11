0e7cad72771deff7ef9633fe14c2228589fa4d42
RasidaSydykova <sydykova.rasida@mail.ru>
2024-04-11 12:37:46 +0600

    Created search form for HeroSection, added icon, refactored code

--- src/features/HomePage/HeroSection/HeroSection.tsx
+++ src/features/HomePage/HeroSection/HeroSection.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link} from "react-router-dom";
 import HeroSectionSwiper from "./components/HeroSectionSwiper/HeroSectionSwiper";
 import HeroSectionCounters from "./components/HeroSectionCounters/HeroSectionCounters";
+import HeroSectionSearchForm from "./components/HeroSectionSearchForm/HeroSectionSearchForm";
 import styles from './HeroSection.module.scss';
 
 const HeroSection = () => {
@@ -9,7 +10,11 @@ const HeroSection = () => {
     <div className={styles.heroSection}>
       <div className={`${styles.heroSection_block} appContainer`}>
         <div className={styles.heroSection_block_content}>
-          <h1 className={styles.heroSection_block_content_title}>Бизнес площадка по ремонту цифровой техники Apple</h1>
+          <h1 className={styles.heroSection_block_content_title}>
+            Бизнес площадка по ремонту цифровой техники Apple и других устройств
+          </h1>
+          {/*Вынесла в отдельный компонет форму, что бы сократить код*/}
+          <HeroSectionSearchForm/>
           <h4 className={styles.heroSection_block_content_subtitle}>Оригинальные запчасти</h4>
           <h4 className={styles.heroSection_block_content_subtitle}>Разумные цены</h4>
           <h4 className={styles.heroSection_block_content_subtitle}>Выезд</h4>
