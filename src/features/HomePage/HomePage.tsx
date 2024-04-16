94c7da707514327ffb00d4f078566c7e112ae867
RasidaSydykova <sydykova.rasida@mail.ru>
2024-04-16 17:44:53 +0600

    Added loader component

--- src/features/HomePage/HomePage.tsx
+++ src/features/HomePage/HomePage.tsx
@@ -3,6 +3,7 @@ import HeroSection from "./HeroSection/HeroSection";
 import WhyChooseUsBlock from "./WhyChooseUsBlock/WhyChooseUsBlock";
 import Order from "../../components/Home/Order";
 import Articles from "../../components/Home/Articles";
+// import Loader from "../../components/Loader/Loader";
 
 const HomePage = () => {
   // Добавила все что было в старом Home, если вдруг сломается
@@ -20,6 +21,9 @@ const HomePage = () => {
       {/*Заменила Depature на WhyChooseUsBlock для оптимизации и ориентировки по стилям*/}
       {/*Заменила вверхний закомментированый блок на HeroSection */}
       {/*Удалила закомментированый блок */}
+
+      {/*Добавила лоадер, что бы при загрузке страницы пользователь понимал, что идет загрузка*/}
+      {/*<Loader />*/}
       <HeroSection/>
       <WhyChooseUsBlock/>
       <Order/>
